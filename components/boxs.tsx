3e4dfa7e010eb201d5cf9de20d9360c7205e8cba
NekkoubOussama <nekkoub.oussama@gmail.com>
2025-04-22 00:19:45 +0100

    littele anime for box

--- components/boxs.tsx
+++ components/boxs.tsx
@@ -7,12 +7,12 @@ type Box1Props = { text: string; title: string; icon: StaticImageData };
 
 export function Box1(props: Box1Props) {
   return (
-    <div className="flex flex-col justify-center gap-2 px-6 py-8 rounded-lg bg-white shadow-2px w-[18.3125rem] animate-fade-left">
+    <div className="flex flex-col justify-center gap-2 px-6 py-8 rounded-lg bg-white shadow-2px w-[18.3125rem] animate-fade-left transform transition-all duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] hover:shadow-xl hover:scale-105 hover:-translate-y-1 hover:ring-1 hover:ring-gray-200">
       <div className="flex flex-col justify-start items-center gap-4 ">
         <Image src={props.icon} alt="icon" />
         <h3>{props.title}</h3>
       </div>
-      <p className=" text-Regularbody3 text-grey">{props.text}</p>
+      <p className=" text-Regularbody3 text-grey ">{props.text}</p>
     </div>
   );
 }
